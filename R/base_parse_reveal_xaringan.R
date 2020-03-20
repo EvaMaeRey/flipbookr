3618dc6249dc91d466fb889da0b8011fa5ba9a86
EvaMaeRey <evangeline.mae@gmail.com>
2020-03-20 17:34:30 -0600

    misspelling dplyr

--- R/base_parse_reveal_xaringan.R
+++ R/base_parse_reveal_xaringan.R
@@ -222,7 +222,7 @@ python_code_full_parse <- function(code){
   code %>%
     code_simple_parse() %>%
     dplyr::mutate(code = raw_code) %>%
-    dplry::mutate(open_par = stringr::str_count(code, "{([")) %>%
+    dplyr::mutate(open_par = stringr::str_count(code, "{([")) %>%
     dplyr::mutate(closed_par = stringr::str_count(code, "})]")) %>%
     dplyr::mutate(auto = cumsum(open_par) == cumsum(closed_par)) %>%
     dplyr::mutate(auto = ifelse(raw_code == "", FALSE, auto)) %>%
